89c12da7ecfd98433447721a5a80778d84fec8b8
tahaanek <rojer.liaw@gmail.com>
2019-10-20 11:03:04 +0700

    updated path call service worker

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator){
   window.addEventListener('load', () => {
   navigator.serviceWorker
-  .register('../sw.js')
+  .register('./sw.js')
   .then(reg => console.log('service registered'))
   .catch(err => console.log('service error : ${error}'));
 });

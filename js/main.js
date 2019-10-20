aea761512b456105d73284d8f321e97666ca25e0
tahaanek <rojer.liaw@gmail.com>
2019-10-20 10:58:38 +0700

    commit  about index service worker

---
+++ js/main.js
@@ -0,0 +1,18 @@
+if('serviceWorker' in navigator){
+  window.addEventListener('load', () => {
+  navigator.serviceWorker
+  .register('../sw.js')
+  .then(reg => console.log('service registered'))
+  .catch(err => console.log('service error : ${error}'));
+});
+
+if('localStorage' in navigator){
+  localStorage.setItem("swCache","service worker chached");
+  var chacheData;
+  chacheData = localStorage.getItem("swCache");
+  document.getElementById("getCacheInput").html(chacheData);
+  }
+
+}else{
+  alert('This browser not support website, please use modern ones');
+}
---
+++ sw.js
@@ -0,0 +1,86 @@
+const cacheApp = 'o-app-v1';
+const cacheData = [
+    'vendor/bootstrap/css/bootstrap.min.css',
+    'vendor/fontawesome-free/css/all.min.css',
+    'vendor/simple-line-icons/css/simple-line-icons.css',
+    'css/landing-page.min.css',
+    'vendor/jquery/jquery.min.js',
+    'vendor/bootstrap/js/bootstrap.bundle.min.js',
+    'img/bg-showcase-1.jpg',
+    'img/bg-showcase-2.jpg',
+    'img/bg-showcase-3.jpg',
+    'img/testimonials-1.jpg',
+    'img/testimonials-2.jpg',
+    'img/testimonials-3.jpg',
+    'index.html',
+    'js/main.js',
+    'about.html',
+];
+// const cart = 
+// {
+//     {
+//     id : 1,
+//     product : 'product 1',
+//     quantity : 2,
+//     priceUnit : 999
+// },
+// {
+//     id : 2,
+//     product : 'product 2',
+//     quantity : 1,
+//     priceUnit : 111
+// }
+// };
+
+
+// install service
+self.addEventListener('install', e => {
+    console.log('service installed');
+
+    e.waitUntil(
+        caches
+        .open(cacheApp)
+        .then(cache => {
+            console.log('service worker chaching file')
+            caches.addAll(cacheData);
+        })
+        .then(() => self.skipWaiting())
+    );
+
+});
+
+// activate service
+self.addEventListener('activate', e => {
+    console.log('service activated');
+    e.waitUntil(
+        caches.keys().then(cacheApp => {
+            return Promise.all(
+                cacheApp.map(cache =>{
+                    if(cache !== cacheApp){
+                      console.log('clearing old cache : '+cacheApp);
+                      return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+// fetch service
+self.addEventListener('fetch', e => {
+    console.log('service fetching');
+    e.respondWith(
+        fetch(e.request)
+          .then(res => {
+            const resClone = res.clone();
+            caches
+            .open(cacheApp)
+            .then(cache => {
+                cache.put(e.request, resClone);
+            });
+            return res;
+          })
+        .catch(() => { 
+            caches.match(e.request).then(res => res) ;
+        })
+      );    
+  });
